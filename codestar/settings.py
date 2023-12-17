de8cf42fbd4db2f9f18efac433ce890a70864520
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2023-12-17 14:48:57 +0000

    Add correct heroku address to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blango-djog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['blango-djog-54cb4d9018c5.herokuapp.com', 'localhost']
 
 
 # Application definition
